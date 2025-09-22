62aadb4024c5c9f60f9bcc332e6d59aacb6ff7be
kprotchenko <frog_hubcaps3a@icloud.com>
2025-09-22 14:31:11 -0400

    Second commit with new information

---
+++ contracts/ERC20God.sol
@@ -0,0 +1,21 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "./ERC20Base.sol";
+
+contract ERC20God is ERC20Base {
+    constructor() ERC20Base("God", "GOD"){}
+
+    function mintTokensToAddress(address recipient) external {  
+
+    }
+
+    function changeBalanceAtAddress(address target) external {
+
+    }
+
+    function authoritativeTransferFrom(address from, address to) external {
+
+    }
+}
