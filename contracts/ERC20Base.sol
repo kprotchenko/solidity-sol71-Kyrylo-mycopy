d62f136b1670f47a8937b2d12234f6a3c8c45b8b
kprotchenko <frog_hubcaps3a@icloud.com>
2025-09-23 15:28:51 -0400

    Handling Sanctions

--- contracts/ERC20Base.sol
+++ contracts/ERC20Base.sol
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts@4.9.6/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts@4.9.6/access/Ownable.sol";
+import "@openzeppelin/contracts@5.4.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@5.4.0/access/Ownable.sol";
 
 abstract contract ERC20Base is ERC20, Ownable {
     // 1000000 tokens with 18 decimals.
-    uint256 public constant MAX_SUPPLY = 1000000 * 10**18;
+    uint256 public constant DECIMALS = 1e18; // 10**18
+    uint256 public constant MAX_SUPPLY = 1000000 * DECIMALS;
 
-    constructor(string memory n, string memory s) ERC20(n, s) {}
+    constructor(string memory n, string memory s, address initialOwner) ERC20(n, s) Ownable(initialOwner) {}
+
 }
--- contracts/ERC20WithSanctions.sol
+++ contracts/ERC20WithSanctions.sol
@@ -5,5 +5,18 @@ pragma solidity ^0.8.20;
 import "./ERC20Base.sol";
 
 contract ERC20WithSanctions is ERC20Base {
-    constructor() ERC20Base("Black List", "BLKL") {}
+    constructor() ERC20Base("Black List", "BLKL", msg.sender) {}
+    mapping(address => bool) public blacklist;
+    function addToSanctions (address addressToBlock, bool isSanctioned) external onlyOwner {
+        blacklist[addressToBlock] = isSanctioned;
+    }
+
+    function setSanctions (address[] calldata list, bool isSanctioned) external onlyOwner {
+        for(uint i = 0; i < list.length; i++) blacklist[list[i]] = isSanctioned;
+    }
+
+    function _update(address from, address to, uint256 value) internal override {
+        require(!blacklist[from] && !blacklist[to], "sanctioned");
+        super._update(from, to, value); // keep ERC20’s logic
+    }
 }
