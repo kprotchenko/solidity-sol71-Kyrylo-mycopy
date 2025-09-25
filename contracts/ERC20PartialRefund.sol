dd67f5607f5068f2e2521fcfb93e14c20ef5ee16
kprotchenko <frog_hubcaps3a@icloud.com>
2025-09-24 21:58:45 -0400

    Continue with ERC20PartialRefund

--- contracts/ERC20PartialRefund.sol
+++ contracts/ERC20PartialRefund.sol
@@ -9,9 +9,11 @@ contract ERC20PartialRefund is ERC20TokenSale {
     uint256  constant ETH_IN_WEI_REFUND_PER_1000_TOKENS = 0.5 ether;      // sellback rate
     function sellBack(uint256 amountOfTokensInDcmls) payable external {
         require(address(this).balance >= amountOfTokensInDcmls*ETH_IN_WEI_REFUND_PER_1000_TOKENS / TOKENS_IN_DCMLS_PER_ETH_IN_WEI,"Not enough balance");
+        require(address(msg.sender).balance >= amountOfTokensInDcmls, "Not enough tokens to sell");
         // since every wei (the smalles unit of ETH) could buy me 1000 smalles decimal parts of one token,
         // I do not have to warry about returning less then 1000 smalles decimal parts of one token
         (bool success, ) = payable(msg.sender).call{value: amountOfTokensInDcmls*ETH_IN_WEI_REFUND_PER_1000_TOKENS / TOKENS_IN_DCMLS_PER_ETH_IN_WEI}("");
+         _burn(msg.sender, amountOfTokensInDcmls);
         require(success, "Refund failed");
     }
 
